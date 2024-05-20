ce578257e2e142007072369a4d955eb09b62206c
devkokora <dev.kokora@gmail.com>
2024-05-21 04:39:11 +0700

    interface1

--- EventManagementSystem/wwwroot/js/site.js
+++ EventManagementSystem/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const search = document.querySelector(".search-index");
+const input = document.querySelector(".input-search-index");
+const btn = document.querySelector(".btn-search-index");
+
+btn.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+});
+
+$(document).ready(function () {
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    $('.counter-count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text().replace(/,/g, '') // Remove any existing commas before animating
+        }, {
+            duration: (Math.random() + 0.5) * 3000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(numberWithCommas(Math.ceil(now)));
+            }
+        });
+    });
+});
